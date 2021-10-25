de2a4c3a86198ce9b02827b3e61a9c27fcd551f1
Lex Liu <alex.liu@treatspace.com>
2021-10-25 15:35:25 -0400

    broken homepage into smaller components and finished styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Homepage from "./homepage.component";
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>Hats</div>
+);
 
 function App() {
   return (
-    <Homepage />
+    <>
+      <Switch>
+        <Route exact path={'/'} component={Homepage} />
+        <Route exact path={'/hats'} component={HatsPage} />
+      </Switch>
+    </>
   );
 }
 
