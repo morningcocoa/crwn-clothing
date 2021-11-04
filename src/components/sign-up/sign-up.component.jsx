f11f897399abd78a1e9e4c1056dc0ca2ed237d0e
Lex Liu <alex.liu@treatspace.com>
2021-11-04 00:26:34 -0400

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -88,7 +88,7 @@ class SignUp extends React.Component {
             label='Confirm Password'
             required
           />
-          <CustomButton type='submit'>SIGN UP</CustomButton>
+          <CustomButton type='submit'>Sign Up</CustomButton>
         </form>
       </div>
     );
